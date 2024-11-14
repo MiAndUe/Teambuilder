873df0409c5f867ef86c4186805143906cf77528
MiAndUe <mhfkeeton@gmail.com>
2024-11-14 13:07:54 -0600

    *Removing the two or more and unknown options in the ethnicity tab*

--- pages/Demographics.vue
+++ pages/Demographics.vue
@@ -101,7 +101,7 @@ export default {
       filters: [
         { name: "Course", options: ["EPCS 2100", "EPCS 2200", "EPCS 3100", "EPCS 3200"], selectedOptions: [] },
         { name: "Enrollment Data", options: ["After Census Day", "After Last Final Exam Day"], selectedOptions: [] },
-        { name: "Ethnicity", options: ["African-American", "Asian", "Hispanic", "International", "Two or more", "Other", "Unknown", "White"], selectedOptions: [] },
+        { name: "Ethnicity", options: ["African-American", "Asian", "Hispanic", "International", "Other", "White"], selectedOptions: [] },
         { name: "Gender", options: ["Female", "Male", "Prefer not to say", "Other"], selectedOptions: [] },
         { name: "Year", options: [], selectedOptions: [] },
         { name: "Semester", options: ["Fall", "Spring", "Summer"], selectedOptions: [] },
